a9ba71c8b134e7de13fa8ab74457632f38709e7c
Maxime G <noemail@noemail.com>
2024-08-17 11:36:31 +0200

    update auto deploy

--- private_dot_config/executable_auto_deploy.sh
+++ private_dot_config/executable_auto_deploy.sh
@@ -5,7 +5,7 @@ sh -c "$(curl --proto '=https' --tlsv1.2 -fsSL https://raw.githubusercontent.com
 
 export PATH="$HOME/.local/bin:$PATH"
 # ho my zsh
-ZSH="$HOME/.oh-my-zsh" SHELL="$HOME/.local/bin/zsh" sh -c "$(curl --proto '=https' --tlsv1.2 -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+ZSH="$HOME/.oh-my-zsh" SHELL="$HOME/.local/bin/zsh" sh -c "$(curl --proto '=https' --tlsv1.2 -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)" -- --unattended
 
 mkdir -p ~/.config/chezmoi
 printf "data:\n    DISPLAY_VAR: :1\n" >> ~/.config/chezmoi/chezmoi.yaml
@@ -15,4 +15,4 @@ curl --proto '=https' --tlsv1.2 -LsSf https://setup.atuin.sh | sh
 
 export TERM=xterm-256color
 
-source ~/.zshrc && omz reload
+zsh -l
