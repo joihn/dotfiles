039988fd90ecfdacf8e62b97c1b080bd13d3ef01
Maxime G <11663917+joihn@users.noreply.github.com>
2024-10-28 14:35:01 +0100

    onchange vim nox

--- run_onchange_install-packages.sh
+++ run_onchange_install-packages.sh
@@ -3,5 +3,5 @@
 # Check if running inside Docker container
 if [ ! -f "/.dockerenv" ]; then
     # Execute only if not in Docker
-    sudo apt update && sudo apt install git vim xclip sshfs wget zsh
+    sudo apt update && sudo apt install git vim-nox xclip sshfs wget zsh
 fi
