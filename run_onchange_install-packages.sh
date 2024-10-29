8063632a5812736884eeab028994e2b4dbf82b9b
Maxime G <11663917+joihn@users.noreply.github.com>
2024-10-29 16:54:41 +0100

    better vim install and mpv

--- run_onchange_install-packages.sh
+++ run_onchange_install-packages.sh
@@ -3,5 +3,14 @@
 # Check if running inside Docker container
 if [ ! -f "/.dockerenv" ]; then
     # Execute only if not in Docker
-    sudo apt update && sudo apt install git vim-nox xclip sshfs wget zsh
+    sudo apt update && sudo apt install git vim-gtk3 xclip sshfs wget zsh 
+fi
+
+# Check if GNOME is installed
+if dpkg -l | grep -q gnome-shell; then
+    # Check if the system architecture is x86-64
+    if [ "$(uname -m)" = "x86_64" ]; then
+        # Install mpv if both conditions are met
+        sudo apt install -y mpv
+    fi
 fi
