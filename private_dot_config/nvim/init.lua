aacbe848713a4e997bd80b78e17ffdd76c469a7c
Maxime.Gardoni <maxime.gardoni@snyk.io>
2026-03-09 20:23:11 +0100

    update 9mars 2026

--- private_dot_config/nvim/init.lua
+++ private_dot_config/nvim/init.lua
@@ -1,2 +1,16 @@
 vim.opt.clipboard:append("unnamedplus")
+return {
+    "nvim-treesitter/nvim-treesitter",
+    build = ":TSUpdate",
+    config = function () 
+      local configs = require("nvim-treesitter.configs")
+
+      configs.setup({
+          ensure_installed = { "javascript", "typescript", "html", "css" },
+          sync_install = false,
+          highlight = { enable = true },
+          indent = { enable = true },  
+        })
+    end
+}
 
