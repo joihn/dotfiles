0556e80b064c9616c4712592608e3e852b011853
Maxime.Gardoni <maxime.gardoni@snyk.io>
2026-06-15 16:14:42 +0200

    Remove anaconda3/etc/conda/activate.d/remove_base_ps1.sh

--- anaconda3/etc/conda/activate.d/literal_remove_base_ps1.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-# Check if the hostname is not 'maximeComputer'
-if [[ "$(hostname)" != "maximeComputer" ]]; then
-  # Execute the desired command
-  PROMPT=$(echo "$PROMPT" | sed 's/(base) //')
-fi
