6894725b6c231d16be9faeaf2e7ad84dda013ee0
Maxime.Gardoni <maxime.gardoni@snyk.io>
2026-06-16 13:43:36 +0200

    init lua mini modif

--- private_dot_config/nvim/init.lua
+++ private_dot_config/nvim/init.lua
@@ -1064,7 +1064,7 @@ require('lazy').setup({
     config = function()
       -- Configure nvim-treesitter properly
       require('nvim-treesitter').setup {
-        ensure_installed = { 'bash', 'c', 'diff', 'html', 'lua', 'luadoc', 'markdown', 'markdown_inline', 'query', 'vim', 'vimdoc' },
+        ensure_installed = { 'bash', 'c', 'css', 'diff', 'html', 'javascript', 'lua', 'luadoc', 'markdown', 'markdown_inline', 'query', 'typescript', 'vim', 'vimdoc' },
         -- Autoinstall languages that are not installed
         auto_install = true,
         highlight = {
@@ -1136,3 +1136,4 @@ require('lazy').setup({
 
 -- The line beneath this is called `modeline`. See `:help modeline`
 -- vim: ts=2 sts=2 sw=2 et
+
